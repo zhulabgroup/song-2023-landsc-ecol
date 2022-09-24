4c5ef58f33c4b388fd492c63489749df2eb64850
yiluansong <yiluansong@gmail.com>
2022-09-24 13:03:07 -0700

    bird and mat

--- empirical/bird/code.R
+++ empirical/bird/code.R
@@ -8,12 +8,15 @@ bird_df<-read_csv("./empirical/bird/data/73_species.csv")  %>%
     Y=YEUREF,
     species_short=Species,
     year=Year,
-    doy=Dayofyear,
+    bh=Dayofyear,
     area=BZ) %>% 
-  mutate(period=case_when(year>=2000~"late",
-                          TRUE~"early")) %>% 
-  mutate(date=as.Date(paste0(year, "-01-01"))+doy-1) %>% 
-  mutate(area=fct_relevel(area, levels=c("HB", "SB", "MB", "NB")))
+  # mutate(date=as.Date(paste0(year, "-01-01"))+bh-1) %>% 
+  mutate(area=fct_relevel(area, levels=c("HB", "SB", "MB", "NB"))) %>% 
+  # group_by(X, Y, species_short, year, area) %>% 
+  # summarise(bh=median(bh, na.rm=T)) %>% 
+  # ungroup() %>% 
+  mutate(period=case_when(year>=1995~"late",
+                          TRUE~"early"))
 
 
 # join with trait data
@@ -29,8 +32,8 @@ bird_df<-bird_df %>%
 
 # project to lon lat
 coord_df<-bird_df %>% 
-  distinct(X, Y) %>% 
-  mutate(coordid=row_number())
+  distinct(X, Y)# %>% 
+  # mutate(coordid=row_number())
 coord_sp<-SpatialPointsDataFrame(coords=coord_df[,c("X", "Y")],data=coord_df,
                                  proj4string = CRS("EPSG:3067"))
 
@@ -41,6 +44,8 @@ coord_df_reproj<-as.data.frame(coord_sp_reproj) %>%
 bird_df<-bird_df %>% 
   left_join(coord_df_reproj, by=c("X", "Y"))
 
+
+###
 sp_vis<-"Phoenicurus phoenicurus"
 
 library(maps)
@@ -50,20 +55,73 @@ world <- map2SpatialPolygons(world, IDs = IDs, proj4string = CRS("+proj=longlat 
 finland<-world["Finland"]
 finland_reproj<-spTransform(finland,CRS("EPSG:3067") )
 
+
+
+### aggregate
+size <- 100000
+# size <- 1.5 # used for supplementary analysis. When doing so, add "_1.5" to file names.
+# size <- 0.5 # used for supplementary analysis. When doing so, add "_0.5" to file names.
+hex_points <- spsample(coord_sp, type = "hexagonal", cellsize = size, offset = c(0, 0))
+hex_grid <- HexPoints2SpatialPolygons(hex_points, dx = size)
+plot(finland_reproj, col = "grey50", bg = "light blue", axes = TRUE)
+plot(hex_points, col = "black", pch = 20, cex = 0.5, add = T)
+plot(hex_grid, border = "orange", add = T)
+
+
+plot_hex <- spatialEco::point.in.poly(coord_sp, hex_grid)
+hex_df<-plot_hex %>% 
+  data.frame() %>% 
+  dplyr::select(X, Y,hexagon=poly.ids) %>% 
+  left_join(hex_points %>% 
+              data.frame() %>% 
+              rename(hex.x=x, hex.y=y) %>% 
+              mutate(hexagon=row_number()),
+            by="hexagon") %>% 
+  mutate(hexagon = as.factor(hexagon)) %>% 
+  mutate(hexagon=fct_shuffle(hexagon))
+
+ggplot() +
+  geom_point(data=hex_df ,aes(x = X, y = Y, col = hexagon)) +
+  geom_polygon(data=hex_grid,aes(x = long, y = lat, group=group),
+               color = "darkblue", fill = NA, size = .1
+  )+
+  theme_classic() +
+  theme(legend.position = "none")
+
 p_map<-ggplot()+
   geom_polygon(
     data = finland_reproj, aes(x = long, y = lat, group = group),
     color = "darkblue", fill = "lightblue", size = .1
   ) +
-  geom_hex(data=bird_df,
-              aes(x=X, y=Y,fill=area,alpha=log(..count..)),bins=50)+
+  geom_polygon(data=hex_grid,aes(x = long, y = lat, group=group),
+               color = "darkblue", fill = NA, size = .1
+  )+
+  # geom_hex(data=bird_df,
+  #          aes(x=X, y=Y,fill=area,alpha=log(..count..)),bins=50)+
+  geom_jitter(data=bird_df %>% filter(species==sp_vis) %>% 
+                mutate(rank=rank(bh)),
+             aes(x=X, y=Y, col=rank), alpha=0.5, cex=0.5)+
   theme_void()+
+  scale_color_viridis_c(direction=-1,
+                        breaks = bird_df %>% 
+                          filter(species==sp_vis) %>% 
+                          mutate(rank=rank(bh)) %>% 
+                          arrange(bh) %>%
+                          mutate(cut = cut(bh,
+                                           breaks = c(min(bh), c( 170, 180, 190), max(bh)),
+                                           include.lowest = T
+                          )) %>%
+                          group_by(cut) %>%
+                          summarise(rank = max(rank) + 0.5) %>%
+                          slice(-n()) %>%
+                          pull(rank),
+                        label = c( 170, 180, 190))+
   # facet_wrap(.~period)+
   coord_equal()+
   # xlab("Latitude")+
   # ylab("Longitude")+
-  labs(alpha="Log of sample size")
-  
+  labs(col="BBS (day of year)")
+
 p_map
 
 ### vipphen SOS
@@ -117,11 +175,42 @@ foreach (file = files,
 sos_df<-bind_rows(sos_df_list)
 sos_df
 write_rds(sos_df, "./empirical/bird/data/sos.rds")
+sos_df_goodcoord<-sos_df %>% 
+  drop_na() %>% 
+  group_by(lon, lat) %>% 
+  summarise(n=n()) %>% 
+  ungroup() %>% 
+  filter(n>=25)
 
+ggplot()+
+  geom_polygon(
+    data = finland, aes(x = long, y = lat, group = group),
+    color = "darkblue", fill = "lightblue", size = .1
+  ) +
+  geom_point(data=sos_df_goodcoord,aes(x=lon, y=lat, col=n))+
+  
+  
+sos_df<-sos_df %>% 
+  right_join(sos_df_goodcoord %>% dplyr::select(lon, lat), by=c("lon", "lat"))
+
+ggplot(sos_df %>% filter(lon==sos_df$lon[1], lat==sos_df$lat[1]))+
+  geom_point(aes(x=year, y=sos))
+
+###
+extent_sp<-extent(coord_df_reproj$lon %>% min()-1,
+                  coord_df_reproj$lon %>% max()+1,
+                  coord_df_reproj$lat %>% min()-1,
+                  coord_df_reproj$lat %>% max()+1) %>% 
+  as('SpatialPolygons') %>% 
+  `projection<-` (CRS("+proj=longlat +datum=WGS84"))
 
 bri<-brick("/data/ZHULAB/phenology/Velocity/TerraClimate/MAT_0.05degree.nc")
+
+cl <- makeCluster(20, outfile = "")
+registerDoSNOW(cl)
+
 mat_df_list<-
-  foreach (year = 1981:2014,
+  foreach (year = (bird_df$year %>% min()):2015,
            .packages = c("gdalUtils", "raster", "tidyverse")) %dopar% {
              
              mat_ras<-bri[[year-1958+1]] %>% 
@@ -136,28 +225,47 @@ mat_df<-bind_rows(mat_df_list)
 mat_df
 write_rds(mat_df, "./empirical/bird/data/mat.rds")
 
-sos_df<-read_rds("./empirical/bird/data/sos.rds")
+# sos_df<-read_rds("./empirical/bird/data/sos.rds")
 mat_df<-read_rds("./empirical/bird/data/mat.rds")
+# year_list<-bird_df$year %>% unique() %>% sort()
 data_df<-bird_df %>% 
-  left_join(sos_df, by=c("lat", "lon", "year")) %>% 
-  left_join(mat_df, by=c("lat", "lon", "year")) %>% 
-  group_by(species,area, year, period, mig) %>% 
-  summarise(sos=median(sos, na.rm=T),
-            mat=median(mat, na.rm=T),
-            bh=quantile(doy, 0.05, na.rm=T),
-            X=median(X),
-            Y=median(Y),
-            n=n()) %>% 
-  ungroup()
+  # group_by(X, Y,species_short,area, period,species, mig, coordid, lon, lat, year) %>% 
+  # summarise(bh=mean(bh)) %>% 
+  dplyr::select(nestid, X, Y, year, bh, area, species, mig, lon, lat) %>% 
+  spread(key="year", value="bh") %>% 
+  gather(key="year", value="bh", -nestid, -X, -Y, -area, -species, -mig, -lon, -lat) %>% 
+  mutate(year=as.numeric(year)) %>% 
+  mutate(period=case_when(year>=1995~"late",
+                          TRUE~"early")) %>% 
+  # left_join(sos_df, by=c("lat", "lon", "year")) %>% 
+  left_join(mat_df, by=c("lat", "lon", "year")) #%>% 
+  # group_by(species,area, year, period, mig) %>% 
+  # summarise(sos=median(sos, na.rm=T),
+  #           mat=median(mat, na.rm=T),
+  #           bh=quantile(bh, 0.05, na.rm=T),
+  #           X=median(X),
+  #           Y=median(Y),
+  #           n=n()) %>% 
+  # ungroup()
+write_rds(data_df, "./empirical/bird/data/data_withcov.rds")
 
-sp_list<-data_df %>% 
+data_agg_df<-data_df %>% 
+  left_join(hex_df, by=c("X", "Y")) %>% 
+  group_by(species, mig, year, period, hexagon, hex.x, hex.y) %>% 
+  summarise(bh=median(bh, na.rm=T),
+            mat=median(mat, na.rm=T),
+            n=n()) %>% 
+  ungroup() %>% 
+  filter(n>=50)
+
+sp_list<-data_agg_df %>% 
   group_by(species, period) %>% 
   drop_na() %>% 
-  summarise(siteyear=n()) %>% 
-  spread(key="period", value="siteyear") %>% 
-  filter(early>=30&late>=30) %>% 
+  summarise(samplesize=n()) %>% 
+  spread(key="period", value="samplesize") %>% 
+  filter(early>=100&late>=100) %>% 
   pull(species)
-data_df<-data_df %>% filter(species %in% sp_list)
+data_agg_df<-data_agg_df %>% filter(species %in% sp_list)
 # ggplot(data_df%>% filter(species==sp_vis) )+
 #   geom_point(aes(x=year, y=bh), alpha=0.3)+
 #   geom_smooth(aes(x=year, y=bh), method="loess")+
@@ -171,29 +279,34 @@ data_df<-data_df %>% filter(species %in% sp_list)
 #   theme_classic()+
 #   facet_wrap(.~species)
 
-ggplot(data_df)+
-  geom_point(aes(x=year, y=bh, col=period), alpha=0.3)+
+ggplot(data_agg_df)+
+  geom_point(aes(x=year, y=bh, col=period), alpha=0.1)+
   geom_smooth(aes(x=year, y=bh), method="lm")+
   theme_classic()+
-  facet_wrap(.~mig*species, scales = "free")
+  facet_wrap(.~paste0(mig,": ",species), scales = "free")
 
 
-ggplot(data_df)+
-  geom_point(aes(x=mat, y=bh, col=period), alpha=0.3)+
+ggplot(data_agg_df )+
+  geom_point(aes(x=mat, y=bh, col=period), alpha=0.1)+
   geom_smooth(aes(x=mat, y=bh,group=period, col=period), method="lm")+
   theme_classic()+
   facet_wrap(.~paste0(mig, ": ",species), scales = "free")
-ggplot(data_df)+
-  geom_point(aes(x=sos, y=bh, col=period), alpha=0.3)+
-  geom_smooth(aes(x=sos, y=bh,group=period, col=period), method="lm")+
-  theme_classic()+
-  facet_wrap(.~paste0(mig, ": ",species), scales = "free")
+# ggplot(data_df)+
+#   geom_point(aes(x=mat, y=sos, col=period), alpha=0.3)+
+#   geom_smooth(aes(x=mat, y=sos,group=period, col=period), method="lm")+
+#   theme_classic()+
+#   facet_wrap(.~paste0(mig, ": ",species), scales = "free")
+# ggplot(data_df)+
+#   geom_point(aes(x=sos, y=bh, col=period), alpha=0.3)+
+#   geom_smooth(aes(x=sos, y=bh,group=period, col=period), method="lm")+
+#   theme_classic()+
+#   facet_wrap(.~paste0(mig, ": ",species), scales = "free")
 
-p_func<-ggplot(data_df %>% filter(species==sp_vis))+
-  geom_point(aes(x=sos, y=bh, col=period), alpha=0.3)+
-  geom_smooth(aes(x=sos, y=bh,group=period, col=period), method="lm")+
+p_func<-ggplot(data_agg_df%>% filter(species==sp_vis))+
+  geom_point(aes(x=mat, y=bh, col=period), alpha=0.1)+
+  geom_smooth(aes(x=mat, y=bh,group=period, col=period), method="lm")+
   theme_classic()+
-  xlab("PGS (day of year)")+
+  xlab("MAT (°C)")+
   ylab("BBS (day of year)")
 p_func
 
@@ -201,10 +314,10 @@ data_df_new_list<-
 foreach (sp = sp_list,
          .packages = c("tidyverse", "spBayes", "gstat", "raster")) %dopar% {
            # spatial regression
-           data_sp<-data_df %>% 
-             filter(species==sp) %>% 
+           data_sp<-data_agg_df %>% 
+             filter(species  == sp) %>% 
              drop_na() %>% 
-             dplyr::select(x=X, y=Y, mat, bh, period, area, species, mig, year)
+             dplyr::select(x=hex.x, y=hex.y, mat, bh, period, species, mig, year)
            ### Fit nonspatial and spatial model
            
            # Prepare data frame
@@ -223,34 +336,32 @@ foreach (sp = sp_list,
              priors <- list("beta.Norm" = list(rep(0, 2), diag(100, 2)), "phi.Unif" = c(-log(0.05) / (phi * 100), -log(0.05) / (phi / 100)), "sigma.sq.IG" = c(2, 2), "tau.sq.IG" = c(2, 0.1)) # shape and scale for IG
              # Set starting and tuning values
              starting <- list("phi" = -log(0.05) / phi, "sigma.sq" = 50, "tau.sq" = 1)
-             tuning <- list("phi" = (log(0.05) / (phi * 100) - log(0.05) / (phi / 100)) / 10, "sigma.sq" = 0.01, "tau.sq" = 0.01)
+             tuning <- list("phi" = (log(0.05) / (phi * 100) - log(0.05) / (phi / 100)) / 10, "sigma.sq" = 0.1, "tau.sq" = 0.1)
              
              # Knots for Gaussian models
-             # knots = kmeans(coords, 20,iter.max=100)$centers
+             # knots = kmeans(data_train[, c("x", "y")] %>% as.matrix(), 10,iter.max=100)$centers
+             knots = c(3,3)
              
              # Run Gaussian spatial model
-             splm <- spLM(bh ~ mat, data = data_train, coords = data_train[, c("x", "y")] %>% as.matrix(), cov.model = "exponential", priors = priors, tuning = tuning, starting = starting, n.samples = 10000, n.report = 1000
-                          # , knots=knots
+             t1<-Sys.time()
+             splm <- spLM(bh ~ mat, data = data_train, coords = data_train[, c("x", "y")] %>% as.matrix(), cov.model = "exponential", priors = priors, tuning = tuning, starting = starting, n.samples = 1000, n.report = 100
+                          , knots=knots
              )
+             t2<-Sys.time()
+             t2-t1
              
-             
-             # ### Parameter inference
-             #   # Recover parameter samples, ignoring the first half of the run as burn-in.
-             #   splm <- spRecover(splm, get.beta = TRUE, get.w = TRUE, start = 5001, n.report = 1000)
-             #   beta.hat <- splm$p.beta.recover.samples
-             #   theta.hat <- splm$p.theta.recover.samples
-             #   
-             #   beta.theta.hat <- cbind(beta.hat, theta.hat)
-             #   # Coefficient summaries
-             #   median <- apply(beta.theta.hat, 2, median)
-             #   lower <- apply(beta.theta.hat, 2, quantile, probs = 0.025)
-             #   upper <- apply(beta.theta.hat, 2, quantile, probs = 0.975)
-             #   
-             #   coef.summary <- data.frame(median, lower, upper)
+             # splm <- spRecover(splm, get.beta = TRUE, get.w = TRUE, start = 1, n.report = 10)
+             # library(ggmcmc)
+             # splm_mcmc1 <- ggs(splm$p.beta.recover.samples)
+             # ggs_traceplot(splm_mcmc1) + theme_classic() + theme(strip.background = element_rect(color = "white", size = 0))
              
              ### Prediction
+             t1<-Sys.time()
              splm.pred <- spPredict(splm, pred.covars=cbind(1, data_sp[, "mat"]) %>% as.matrix(), pred.coords=data_sp[, c("x", "y")] %>% as.matrix(),
-                                    start=5001, thin = 10)
+                                    start=501)
+             t2<-Sys.time()
+             t2-t1
+             
              quant <- function(x){quantile(x, prob=c(0.025, 0.5, 0.975))}
              y.hat <- apply(splm.pred$p.y.predictive.samples, 1, quant) %>% 
                t() %>% 
@@ -283,26 +394,28 @@ data_mis<-read_rds("./empirical/bird/data/mismatch_mat.rds")
 
 # plot mismatch
 p_pred<-ggplot(data_mis  %>% filter(species==sp_vis))+
-  geom_point(aes(x=predict, y=bh))+
-  geom_errorbarh(aes(y=bh, xmin=lower, xmax=upper), alpha=0.5)+
+  geom_point(aes(x=predict, y=bh), alpha=0.1)+
+  # geom_errorbarh(aes(y=bh, xmin=lower, xmax=upper), alpha=0.5)+
+  # geom_smooth(aes(x=predict, y=bh), method="lm")+
   geom_abline(intercept = 0, slope=1, col="red")+
-  ggpubr::stat_cor(
-    aes(
-      x=predict, y=bh,
-      label = paste( ..rr.label..,..p.label.., sep = "*`,`~")
-    ),
-    p.accuracy = 0.05,
-    label.x.npc = "left",
-    label.y.npc = "top",
-    show.legend = F,
-    col = "blue"
-  ) +
+  # ggpubr::stat_cor(
+  #   aes(
+  #     x=predict, y=bh,
+  #     label = paste( ..rr.label..,..p.label.., sep = "*`,`~")
+  #   ),
+  #   p.accuracy = 0.05,
+  #   label.x.npc = "left",
+  #   label.y.npc = "top",
+  #   show.legend = F,
+  #   col = "blue"
+  # ) +
   theme_classic()+
   facet_wrap(.~period, nrow=1)+
   guides(col="none")+
   xlab("Predicted BBS (day of year)")+
   ylab("Observed BBS (day of year)")+
-  coord_equal()
+  # coord_equal()+
+  scale_fill_viridis_c()
 p_pred
 
 # test if mismatch is different from 0
@@ -341,6 +454,40 @@ p_mis<-ggplot()+
   ylab("Difference between observed and predicted BBS (day)")+
   xlab ("Species")
 p_mis
+
+
+ggplot(data_mis %>% filter(period=="late") %>%
+         left_join(t_df, by="species") %>%
+         filter(estimate<0 & p<0.05) )+
+  # geom_point(aes(x=y, y=resid, col=species), alpha=0.1)+
+  geom_smooth(aes(x=y, y=resid, col=species), method="lm")+
+  ggpubr::stat_cor(
+    aes(
+      x=y, y=resid,
+      label = paste( ..rr.label..,..p.label.., sep = "*`,`~")
+    ),
+    p.accuracy = 0.05,
+    label.x.npc = "left",
+    label.y.npc = "top",
+    show.legend = F,
+    col = "blue"
+  ) +
+  geom_hline(yintercept = 0, lty=2)+
+  theme_classic()+
+  # facet_wrap(.~species, scales="free")+
+  guides(col="none")
+
+library(nlme)
+# lme.fit <- lme(resid ~ y, random = ~ 1 + y | species, data = data_mis %>% filter(period=="late"))
+# summary(lme.fit)
+lme.fit <- lme(abs(resid) ~ y, random = ~ 1+y | species,
+               # corr = corSpatial(form = ~x + y, type ="exponential", nugget = T),
+               data = data_mis %>% filter(period=="late") %>%
+                 # left_join(t_df, by="species") %>%
+                 # filter(estimate<0 & p<0.05) %>% 
+                 mutate(x=jitter(x, amount=0.000001), y=jitter(y, amount=0.000001)) ,
+               control = lmeControl(opt = "optim", optimMethod = "SANN"))
+summary(lme.fit)
 
 # data_mis_summ<-data_mis %>% 
 #   group_by(species, period, mig, area) %>% 
@@ -386,13 +533,13 @@ grid.arrange(annotate_figure(p_map, fig.lab = "(a)"),
              annotate_figure(p_func, fig.lab = "(b)"),
              annotate_figure(p_pred, fig.lab = "(c)"),
              annotate_figure(p_mis, fig.lab = "(d)"),
-             annotate_figure(p_area, fig.lab = "(e)"),
+             # annotate_figure(p_area, fig.lab = "(e)"),
              layout_matrix = rbind(
                c(1, 4),
                c(2, 4),
-               c(3, 5)
+               c(3, 4)
              ),
              widths=c(2,3),
-             heights=c(3,3,2)
+             heights=c(3,2,2)
 )
 dev.off()
